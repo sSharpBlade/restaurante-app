a61648f8313a8eec7d7b4505323178ff93399dc8
SharpBlade <yepezedwin760@gmail.com>
2024-08-02 16:03:46 -0500

    Commit inicial

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e8e8e8',
     alignItems: 'center',
     justifyContent: 'center',
   },
