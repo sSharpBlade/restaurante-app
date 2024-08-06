4c1d1d391d97f5f76919967548389ba74b1c6af0
SharpBlade <yepezedwin760@gmail.com>
2024-08-05 21:24:12 -0500

    ruta e icono de perfil

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,18 +1,31 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs, Redirect } from "expo-router";
-import { HomeIcon } from "../../components/Icons";
+import { HomeIcon, ProfileIcon } from "../../components/Icons";
 
 const TabsLayout = () => {
   return (
     <>
-      <Tabs screenOptions={{ tabBarShowLabel: false }}>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+        }}
+      >
         <Tabs.Screen
           name="home"
           options={{
             title: "Home",
             headerShown: false,
             tabBarIcon: () => HomeIcon(),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: () => ProfileIcon(),
           }}
         />
       </Tabs>
--- components/Icons.jsx
+++ components/Icons.jsx
@@ -3,3 +3,7 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 export const HomeIcon = (props) => (
   <Ionicons name="home" size={24} color="black" {...props} />
 );
+
+export const ProfileIcon = (props) => (
+  <Ionicons name="person-circle" size={30} color="black" />
+);
