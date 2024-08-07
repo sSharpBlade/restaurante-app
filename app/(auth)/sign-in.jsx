338972a1c8264184b4d7816d7fc3cefd7f62de83
SharpBlade <yepezedwin760@gmail.com>
2024-08-06 20:53:45 -0500

    Más acciones en iniciar

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -2,12 +2,17 @@ import { View, Text, ScrollView } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const submit = () => {};
 
   return (
     <SafeAreaView className="h-full bg-black">
@@ -26,6 +31,17 @@ const SignIn = () => {
             handleChangeText={(p) => setForm({ ...form, password: p })}
             otherStyles="mt-7"
           />
+          <CustomButton
+            title="Ingresar"
+            handlePress={submit}
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 ">Don't have account?</Text>
+            <Link href="/sign-up" className="text-lg text-blue-300">
+              Sign up
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
