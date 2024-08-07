f02eca43357465facbd00f4a1e73843d5a57eaf4
SharpBlade <yepezedwin760@gmail.com>
2024-08-06 21:02:16 -0500

    Formulario para registro

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,59 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const submit = () => {};
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className="h-full bg-black">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <FormField
+            title={"Username"}
+            value={form.username}
+            handleChangeText={(u) => setForm({ ...form, username: u })}
+            otherStyles="mt-7"
+          />
+          <FormField
+            title={"Email"}
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title={"Password"}
+            value={form.password}
+            handleChangeText={(p) => setForm({ ...form, password: p })}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign up"
+            handlePress={submit}
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 ">
+              Have an account already?
+            </Text>
+            <Link href="/sign-in" className="text-lg text-blue-300">
+              Sign in
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
