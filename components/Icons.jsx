1a9095d7cf0f4b0147d2deb400ad00b94d082867
SharpBlade <yepezedwin760@gmail.com>
2024-08-06 20:32:07 -0500

    Icono para mostrar contraseña

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -1,4 +1,5 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export const HomeIcon = (props) => (
   <Ionicons name="home" size={24} color="black" {...props} />
@@ -6,4 +7,14 @@ export const HomeIcon = (props) => (
 
 export const ProfileIcon = (props) => (
   <Ionicons name="person-circle" size={30} color="black" />
+);
+
+export const showPasswordIcon = (props) => (
+  <FontAwesome
+    name="low-vision"
+    size={24}
+    color="white"
+    {...props}
+    className="w-6 h-6"
+  />
 );
