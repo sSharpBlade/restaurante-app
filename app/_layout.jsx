21f39c9cd941beb1024957e803fdf829f8a826be
SharpBlade <yepezedwin760@gmail.com>
2024-08-06 20:57:49 -0500

    Cambios minimos en las rutas

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,6 +5,9 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
     </Stack>
   );
 }
