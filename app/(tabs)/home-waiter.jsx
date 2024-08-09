bae8e2d258973e73a91f9d921fa804a3793fe034
SharpBlade <yepezedwin760@gmail.com>
2024-08-08 23:16:19 -0500

    interfaz de mesero

---
+++ app/(tabs)/home-waiter.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeWaiter = () => {
+  return (
+    <View>
+      <Text>HomeWaiter</Text>
+    </View>
+  );
+};
+
+export default HomeWaiter;
